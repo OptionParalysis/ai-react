59e582f855ea3098ac3633f68ab4e7807288414e
Zach Janice <zach.janice@intouchsol.com>
2025-08-27 09:28:44 -0500

    setup prettier

--- index.ts
+++ index.ts
@@ -1,16 +1,16 @@
 import concurrently from 'concurrently'
 
 concurrently([
-    {
-        name: 'sever',
-        command: 'bun run dev',
-        cwd: 'packages/server',
-        prefixColor: 'cyan'
-    },
-    {
-        name: 'client',
-        command: 'bun run dev',
-        cwd: 'packages/client',
-        prefixColor: 'green' 
-    }
+  {
+    name: 'sever',
+    command: 'bun run dev',
+    cwd: 'packages/server',
+    prefixColor: 'cyan',
+  },
+  {
+    name: 'client',
+    command: 'bun run dev',
+    cwd: 'packages/client',
+    prefixColor: 'green',
+  },
 ])
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -5,18 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:3000'
-    }
-  }
+      '/api': 'http://localhost:3000',
+    },
+  },
 })
